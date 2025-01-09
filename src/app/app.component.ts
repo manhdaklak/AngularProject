81a8a2b9cdcc50bf06dfec253c88c76aaae83a6b
Mạnh Nguyễn <manh0974@gmail.com>
2025-01-09 23:06:31 +0700

    Angular setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Project-Angular-New';
+  title = 'Project-Angular-Test';
 }
